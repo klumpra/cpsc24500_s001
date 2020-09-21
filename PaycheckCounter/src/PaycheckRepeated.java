3333f6de580928aaab620cdbe27d2f0f6647304e
Ray Klump <klumpra@lewisu.edu>
2020-09-21 13:34:08 -0500

    Version of paycheck counter with while loop

--- PaycheckCounter/src/PaycheckRepeated.java
+++ PaycheckCounter/src/PaycheckRepeated.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class PaycheckRepeated {
 	public static void main(String[] args) {
-		System.out.print("How many employees: ");
 		Scanner sc = new Scanner(System.in);
-		int empCount;
 		double hours,rate,gross;
-		empCount = sc.nextInt();
-		for (int i = 0; i < empCount; i++) {
+		String doAgain = "y";
+		while (doAgain.equalsIgnoreCase("y")) {
 			System.out.print("Enter hourly rate and hours worked: ");
 			rate = sc.nextDouble();
 			hours = sc.nextDouble();
 			gross = hours*rate;
 			System.out.printf("Gross pay: $%.2f.\n",gross);
+			System.out.print("Again? ");
+			doAgain = sc.next().trim();  // gets the user input and trims whitespace
 		}
 		System.out.println("Thank you.");
 	}
