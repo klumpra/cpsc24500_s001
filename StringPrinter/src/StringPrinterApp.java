cc64bb80796a3a35249665fbd22a419cc4155dc9
Ray Klump <klumpra@lewisu.edu>
2020-10-26 13:59:58 -0500

    Finished StringPrinter

--- StringPrinter/src/StringPrinterApp.java
+++ StringPrinter/src/StringPrinterApp.java
@@ -1,31 +1,47 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 class StringPanel extends JPanel {
 	private String text; // the text to show
+	private Font font;
 	public void setText(String s) {
 		text = s;
 	}
 	public String getText() {
 		return text;
 	}
+	public void setFont(String name, int size) {
+		if (size < 6) {
+			size = 6;
+		} 
+		font = new Font(name,Font.BOLD,size);
+	}
+	
 	public StringPanel() {
 		text = "Welcome!";
+		font = new Font("Arial",Font.PLAIN,12);
 	}
 	public StringPanel(String s) {
 		text = s;
+		font = new Font("Arial",Font.PLAIN,12);
 	}
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		g.setFont(font);
 		g.drawString(text, 20, 50);
 	}
 }
@@ -36,6 +52,7 @@ class StringPrinterFrame extends JFrame {
 		Container c = getContentPane();
 		c.setLayout(new BorderLayout());
 		StringPanel panCenter = new StringPanel();
+		panCenter.setText("It is only Monday and that sucks.");
 		c.add(panCenter,BorderLayout.CENTER);
 		JPanel panSouth = new JPanel();
 		panSouth.setLayout(new FlowLayout());
@@ -47,7 +64,18 @@ class StringPrinterFrame extends JFrame {
 		panSouth.add(lblFontSize);
 		JTextField txtFontSize = new JTextField(2);
 		panSouth.add(txtFontSize);
+		JTextField txtFontName = new JTextField(10);
+		panSouth.add(txtFontName);
 		JButton btnChange = new JButton("Change");
+		btnChange.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panCenter.setText(txtStringToShow.getText());
+				panCenter.setFont(txtFontName.getText(), 
+						Integer.parseInt(txtFontSize.getText()));
+				repaint();  // forces the whole window to repaint.
+			}
+		}		
+		);
 		panSouth.add(btnChange);
 		c.add(panSouth,BorderLayout.SOUTH);
 	}
