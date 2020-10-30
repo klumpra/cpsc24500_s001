a58e27a43973d8a3dc182c92eed9c4398f157d65
Ray Klump <klumpra@lewisu.edu>
2020-10-30 13:51:39 -0500

    Finished responding to mouse events. Next up - drawing as we drag the mouse.

--- MenuAndMouseEvents/src/MenuAndMouseApp.java
+++ MenuAndMouseEvents/src/MenuAndMouseApp.java
@@ -4,6 +4,9 @@ import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,10 +18,12 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-class DrawingPanel extends JPanel {
+class DrawingPanel extends JPanel implements MouseListener, MouseMotionListener {
 	private String mouseStatus;
 	public DrawingPanel() {
 		mouseStatus = "Welcome";
+		addMouseListener(this);
+		addMouseMotionListener(this);
 	}
 	@Override
 	public void paintComponent(Graphics g) {
@@ -31,6 +36,42 @@ class DrawingPanel extends JPanel {
 	public void setMouseStatus(String ms) {
 		mouseStatus = ms;
 	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		mouseStatus = "Mouse entered the panel.";
+		repaint();
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		mouseStatus = "Mouse exited the panel.";
+		repaint();
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		mouseStatus = String.format("mouse clicked at (%d, %d)", e.getX(),e.getY());
+		repaint();
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		mouseStatus = String.format("mouse pressed at (%d, %d)", e.getX(),e.getY());
+		repaint();
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		mouseStatus = String.format("mouse released at (%d, %d)", e.getX(),e.getY());
+		repaint();
+	}
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		mouseStatus = String.format("mouse moving at (%d, %d)", e.getX(),e.getY());
+		repaint();
+	}
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		mouseStatus = String.format("mouse dragging at (%d, %d)", e.getX(),e.getY());
+		repaint();
+	}
+	
 }
 class MenuAndMouseFrame extends JFrame {
 	private DrawingPanel pan;
